3becbfa377bdcffb473a03c37382a2a9b3434198
YixuanGao98 <73766690+YixuanGao98@users.noreply.github.com>
2025-04-11 10:57:32 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import torch
 import torchvision.transforms as transforms
 from PIL import Image
 import os
-
+import numpy as np
 # Assuming the model definition is already available, import your trained model class
 from main import pil_loader,RichIQA
 
@@ -35,10 +35,13 @@ def predict_image_quality(model, image_path):
     # Run inference
     with torch.no_grad():
         output = model(image)
-    
+        
     # Assuming the output is a quality score, convert it to a readable format
-    quality_score = output.item()  # Modify if output is not a single value
-    return quality_score
+    quality_score = output.cpu().detach().numpy()  # Modify if output is not a single value
+    si = np.arange(1, 6, 1)#
+    # si = np.arange(5, 105, 10)#
+    mean = np.sum(quality_score * si)
+    return quality_score,mean
 
 if __name__ == "__main__":
     # Load the model
@@ -48,7 +51,9 @@ if __name__ == "__main__":
     image_path = 'test.jpg'  # Update this path to your input image
 
     # Predict the image quality
-    quality_score = predict_image_quality(model, image_path)
+    quality_score,mean = predict_image_quality(model, image_path)
 
     # Print the result
-    print(f'Predicted Image Quality Score: {quality_score}')
+    print(f'Predicted Image Quality Score  Distribution: {quality_score}')
+    print(f'Predicted mos: {mean}')
+
